080fdd01e2528b48ead30a01458bbcd9fe58f582
zihad07 <zihadnayem8gmail.com>
2019-12-04 21:20:19 +0600

    Complete one to one crud operation

--- onetoone/database/migrations/2019_12_04_142432_create_addresses_table.php
+++ onetoone/database/migrations/2019_12_04_142432_create_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned()->nullable();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 }
--- onetoone/routes/web.php
+++ onetoone/routes/web.php
@@ -11,6 +11,42 @@
 |
 */
 
+use App\Address;
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insert',function(){
+    $user = User::findOrFail(2);
+
+    $address = new Address(['name'=>'9900 NK BD.']);
+
+    $user->address()->save($address);
+});
+
+
+Route::get('/update',function(){
+    $address = Address::whereUserId(2)->first();
+
+    $address->name = '5657 udated version address';
+    $address->save();
+
+});
+
+Route::get('/read',function(){
+    $user = User::findOrFail(2);
+
+    $address = $user->address->name;
+    return $user->name.' address is '.$address;
+});
+
+Route::get('/delete',function(){
+    $user = User::findOrFail(2);
+
+    $user->address()->delete();
+    // $user->address()->first()->delete();
+    return $user->name.'Address Delete';
+});
+
